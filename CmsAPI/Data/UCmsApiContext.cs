645dfb96c79486fd570cb3c8cac25d8b9f9d3153
zokirobi <z1231986@gmail.com>
2018-03-28 14:29:43 +0200

    initial

---
+++ CmsAPI/Data/UCmsApiContext.cs
@@ -0,0 +1,17 @@
+﻿using CmsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsAPI.Data
+{
+    public class UCmsApiContext : DbContext
+    {
+        public UCmsApiContext(DbContextOptions<UCmsApiContext> opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<SideBar> SideBar { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ CmsAPI/Models/SideBar.cs
@@ -0,0 +1,8 @@
+﻿namespace CmsAPI.Models
+{
+    public class SideBar
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
